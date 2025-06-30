139835d7fc8d06e9bc43b8a41b793327bb846db9
Gil Bregman <gbregman@il.ibm.com>
2025-06-30 20:09:57 +0300

    Do not raise an exception when asking to remove an already removed image metadata. Fixes #1392
    Signed-off-by: Gil Bregman <gbregman@il.ibm.com>

--- tests/ha/auto_resize.sh
+++ tests/ha/auto_resize.sh
@@ -42,6 +42,37 @@ if [[ $? -eq 0 ]]; then
 fi
 set -e
 
+echo "ℹ️  set namespaces auto-resize flag to same value"
+cephnvmf_func namespace set_auto_resize --subsystem ${NQN} --nsid 1 --auto-resize-enabled no
+cephnvmf_func namespace set_auto_resize --subsystem ${NQN} --nsid 2 --auto-resize-enabled yes
+
+echo "ℹ️  namespaces list should be identical"
+ns_list=$(cephnvmf_func --output stdio --format json namespace list --subsystem ${NQN})
+[[ `echo $ns_list | jq -r '.status'` == "0" ]]
+[[ `echo $ns_list | jq -r '.subsystem_nqn'` == "${NQN}" ]]
+[[ `echo $ns_list | jq -r '.namespaces[0].nsid'` == "1" ]]
+[[ `echo $ns_list | jq -r '.namespaces[0].rbd_image_name'` == "${RBD_IMAGE_NAME}" ]]
+[[ `echo $ns_list | jq -r '.namespaces[0].rbd_pool_name'` == "${RBD_POOL}" ]]
+[[ `echo $ns_list | jq -r '.namespaces[0].rbd_image_size'` == "10485760" ]]
+[[ `echo $ns_list | jq -r '.namespaces[0].disable_auto_resize'` == "true" ]]
+[[ `echo $ns_list | jq -r '.namespaces[1].nsid'` == "2" ]]
+[[ `echo $ns_list | jq -r '.namespaces[1].rbd_image_name'` == "${RBD_IMAGE_NAME}2" ]]
+[[ `echo $ns_list | jq -r '.namespaces[1].rbd_pool_name'` == "${RBD_POOL}" ]]
+[[ `echo $ns_list | jq -r '.namespaces[1].rbd_image_size'` == "10485760" ]]
+[[ `echo $ns_list | jq -r '.namespaces[1].disable_auto_resize'` == "false" ]]
+[[ `echo $ns_list | jq -r '.namespaces[2]'` == "null" ]]
+
+echo "ℹ️  RBD metadata should be identical"
+rbd_meta=`make -s exec SVC=ceph OPTS=-T CMD="rbd image-meta get -p ${RBD_POOL} ${RBD_IMAGE_NAME} NVME_GATEWAY_AUTO_RESIZE" 2> /dev/null`
+[[ "$rbd_meta" == "no" ]]
+set +e
+make -s exec SVC=ceph OPTS=-T CMD="rbd image-meta get -p ${RBD_POOL} ${RBD_IMAGE_NAME}2 NVME_GATEWAY_AUTO_RESIZE" 2> /dev/null
+if [[ $? -eq 0 ]]; then
+    echo "Shouldn't have NVME_GATEWAY_AUTO_RESIZE in metadata for ${RBD_POOL}/${RBD_IMAGE_NAME}2"
+    exit 1
+fi
+set -e
+
 echo "ℹ️  resize RBD images"
 make -s exec SVC=ceph OPTS=-T CMD="rbd resize -p ${RBD_POOL} ${RBD_IMAGE_NAME} --size 20M"
 make -s exec SVC=ceph OPTS=-T CMD="rbd resize -p ${RBD_POOL} ${RBD_IMAGE_NAME}2 --size 20M"
